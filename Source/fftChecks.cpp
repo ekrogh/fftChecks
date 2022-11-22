2691993e40cc6832b703cefa2d9f45ccb94117ff
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-11-22 17:14:18 +0100

    data containers under way.

--- Source/fftChecks.cpp
+++ Source/fftChecks.cpp
@@ -27,52 +27,67 @@
 //[/MiscUserDefs]
 
 //==============================================================================
-fftChecks::fftChecks ()
+fftChecks::fftChecks()
+	: m_plot{ cmp::Plot() }
 {
-    //[Constructor_pre] You can add your own custom stuff here..
-    //[/Constructor_pre]
+	//[Constructor_pre] You can add your own custom stuff here..
+	//[/Constructor_pre]
 
 
-    //[UserPreSize]
-    //[/UserPreSize]
+	//[UserPreSize]
+	//[/UserPreSize]
 
-    setSize (600, 400);
+	setSize(1200, 800);
 
 
-    //[Constructor] You can add your own custom stuff here..
-    //[/Constructor]
+	//[Constructor] You can add your own custom stuff here..
+
+	// Add the plot object as a child component.
+	addAndMakeVisible(m_plot);
+
+	// Plot some values.
+	m_plot.plot({ fftvctr });
+	//m_plot.plot({ {1, 3, 7, 9, 13} });
+
+	//m_plot.setXTicks({ 1.7f, 2.0f, 3.23f, 4.0f, 5.3f });
+
+	//[/Constructor]
 }
 
 fftChecks::~fftChecks()
 {
-    //[Destructor_pre]. You can add your own custom destruction code here..
-    //[/Destructor_pre]
+	//[Destructor_pre]. You can add your own custom destruction code here..
+	//[/Destructor_pre]
 
 
 
-    //[Destructor]. You can add your own custom destruction code here..
-    //[/Destructor]
+	//[Destructor]. You can add your own custom destruction code here..
+	//[/Destructor]
 }
 
 //==============================================================================
-void fftChecks::paint (juce::Graphics& g)
+void fftChecks::paint(juce::Graphics& g)
 {
-    //[UserPrePaint] Add your own custom painting code here..
-    //[/UserPrePaint]
+	//[UserPrePaint] Add your own custom painting code here..
+	//[/UserPrePaint]
 
-    g.fillAll (juce::Colour (0xff505050));
+	g.fillAll(juce::Colour(0xff505050));
 
-    //[UserPaint] Add your own custom painting code here..
-    //[/UserPaint]
+	//[UserPaint] Add your own custom painting code here..
+	//[/UserPaint]
 }
 
 void fftChecks::resized()
 {
-    //[UserPreResize] Add your own custom resize code here..
-    //[/UserPreResize]
+	//[UserPreResize] Add your own custom resize code here..
+	//[/UserPreResize]
 
-    //[UserResized] Add your own custom resize handling here..
-    //[/UserResized]
+	//[UserResized] Add your own custom resize handling here..
+	
+	// Set the bounds of the plot to fill the whole window.
+	m_plot.setBounds(getBounds());
+
+	//[/UserResized]
 }
 
 
@@ -85,15 +100,15 @@ void fftChecks::resized()
 #if 0
 /*  -- Projucer information section --
 
-    This is where the Projucer stores the metadata that describe this GUI layout, so
-    make changes in here at your peril!
+	This is where the Projucer stores the metadata that describe this GUI layout, so
+	make changes in here at your peril!
 
 BEGIN_JUCER_METADATA
 
 <JUCER_COMPONENT documentType="Component" className="fftChecks" componentName=""
-                 parentClasses="public juce::Component" constructorParams="" variableInitialisers=""
-                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
-                 fixedSize="0" initialWidth="600" initialHeight="400">
+				 parentClasses="public juce::Component" constructorParams="" variableInitialisers="m_plot{ cmp::Plot() }"
+				 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+				 fixedSize="0" initialWidth="1200" initialHeight="800">
   <BACKGROUND backgroundColour="ff505050"/>
 </JUCER_COMPONENT>
 
--- Source/fftChecks.h
+++ Source/fftChecks.h
@@ -21,47 +21,62 @@
 
 //[Headers]     -- You can add your own extra header files here --
 #include <JuceHeader.h>
+
 #include "cmp_plot.h"
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
 //[/Headers]
 
 
 
 //==============================================================================
 /**
-																	//[Comments]
+                                                                    //[Comments]
 	An auto-generated component, created by the Projucer.
 
 	Describe your class and how it works here!
-																	//[/Comments]
+                                                                    //[/Comments]
 */
-class fftChecks : public juce::Component
+class fftChecks  : public juce::Component
 {
-	// Declare plot object.
-	cmp::Plot m_plot;
-
 public:
-	//==============================================================================
-	fftChecks();
-	~fftChecks() override;
+    //==============================================================================
+    fftChecks ();
+    ~fftChecks() override;
 
-	//==============================================================================
-	//[UserMethods]     -- You can add your own custom methods in this section.
-	//[/UserMethods]
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    enum
+    {
+        fftOrderAtStart = 11
+    };
+    //[/UserMethods]
 
-	void paint(juce::Graphics& g) override;
-	void resized() override;
+    void paint (juce::Graphics& g) override;
+    void resized() override;
 
 
 
 private:
-	//[UserVariables]   -- You can add your own custom variables in this section.
-	//[/UserVariables]
+    //[UserVariables]   -- You can add your own custom variables in this section.
+	// Declare plot object.
+	cmp::Plot m_plot;
 
-	//==============================================================================
+    std::unique_ptr<juce::dsp::FFT> forwardFFT = std::make_unique<juce::dsp::FFT>(fftOrderAtStart);
+
+    vector<float> fftvctr = *(new vector<float>(2048, 0.0));
+    float* fftbfr = fftvctr.data();
+    //[/UserVariables]
+
+    //==============================================================================
 
 
-	//==============================================================================
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(fftChecks)
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (fftChecks)
 };
 
 //[EndFile] You can add extra defines here...
